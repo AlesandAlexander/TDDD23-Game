f522e92b4dc73b76ff1c0c435ba00f5a3222cdba
Alexander Alesand <alexander.alesand@gmail.com>
2014-09-21 20:15:22 +0200

    background added

--- background.lua
+++ background.lua
@@ -1,5 +1,39 @@
-local background
+local image = display.newImageRect( "bg.png", _W*9, _H )
+image.anchorX=1
+image.x = _W
+image.y =_H/2
+local rotationSpeed = 120
+local scrollSpeed = 0
 
 
+function image:rotateRight()
+	scrollSpeed = -rotationSpeed
+	local function stopRotation()
+		scrollSpeed = 0
+	end
+	timer.performWithDelay( 120, stopRotation)
+end	
 
-return background
+function image:rotateLeft()
+	scrollSpeed = rotationSpeed
+	local function stopRotation()
+		scrollSpeed = 0
+	end
+	timer.performWithDelay( 120, stopRotation)
+end	
+
+
+local function scroll(event)
+	image.x = image.x + scrollSpeed
+	if image.x > _W*9 then
+		image.x = _W
+	end
+	if image.x < _W then
+		image.x = _W*9
+	end
+end
+
+Runtime:addEventListener( "enterFrame", scroll )
+
+
+return image
---
+++ backgroundSprite.lua
@@ -0,0 +1,64 @@
+local options =
+{ 
+	frames = 
+	{
+		{
+			x=0,
+			y=0,
+			width=160,
+			height=240
+		},
+		
+		{
+			x=160,
+			y=0,
+			width=160,
+			height=240
+		},
+		
+		{
+			x=320,
+			y=0,
+			width=160,
+			height=240
+		},
+
+		{
+			x=480,
+			y=0,
+			width=160,
+			height=240
+		},
+		
+
+		{
+			x=640,
+			y=0,
+			width=160,
+			height=240
+		},
+
+		{
+			x=800,
+			y=0,
+			width=160,
+			height=240
+		},
+		
+		{
+			x=960,
+			y=0,
+			width=160,
+			height=240
+		},
+
+		{
+			x=1120,
+			y=0,
+			width=160,
+			height=240
+		}
+		
+	}
+}
+return options
